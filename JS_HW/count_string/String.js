3d649b9a3623347c0826c7650d0baa53335a43ef
yeon0425 <98527100+yeon0425@users.noreply.github.com>
2022-05-06 14:33:10 +0900

    Delete JS_HW directory

--- JS_HW/count_string/String.js
+++
@@ -1,9 +0,0 @@
-function countString() {
-  let content = document.getElementById("text").value;
-
-  if (content.length > 200) {
-    content = content.substring(0, 10);
-    document.getElementById("text").value = content;
-  }
-  document.getElementById("count").innerHTML = content.length;
-}
